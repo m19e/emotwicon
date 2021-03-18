d1ade3713899669c2a9b55d31dcd0dcd763d9914
m19e <nexus.me7290@gmail.com>
2021-03-18 17:37:55 +0900

    Add stylelint config

---
+++ stylelint.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: ["stylelint-config-recommended"],
+    rules: {
+        "at-rule-no-unknown": [
+            true,
+            {
+                ignoreAtRules: ["extends", "tailwind"],
+            },
+        ],
+        "block-no-empty": null,
+        "unit-whitelist": ["em", "rem", "s"],
+    },
+};
