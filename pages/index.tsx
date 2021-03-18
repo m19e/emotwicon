75d0cd48d8c8bd1f32d19c9b5790bb5b9e36d93d
m19e <nexus.me7290@gmail.com>
2021-03-19 03:51:39 +0900

    Get, pass request url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
+import { GetServerSidePropsContext, GetServerSidePropsResult } from "next";
 import Head from "next/head";
 
-const Home = () => (
+type Props = {
+    url: string;
+};
+
+const Home = ({ url }: Props) => (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <Head>
             <title>Create Next App</title>
@@ -17,6 +22,10 @@ const Home = () => (
 
             <p className="mt-3 text-2xl">
                 Get started by editing <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">pages/index.js</code>
+            </p>
+
+            <p className="mt-3 text-2xl">
+                <code>{url}</code>
             </p>
 
             <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
@@ -60,5 +69,14 @@ const Home = () => (
         </footer>
     </div>
 );
+
+export const getServerSideProps = ({ req }: GetServerSidePropsContext): GetServerSidePropsResult<Props> => {
+    const { url } = req;
+    return {
+        props: {
+            url,
+        },
+    };
+};
 
 export default Home;
