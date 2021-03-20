758d5bfdbdf53ebc990d86b8b3e7576ed0f91963
m19e <nexus.me7290@gmail.com>
2021-03-20 11:55:11 +0900

    Add sample stamps

--- constants/stampList.ts
+++ constants/stampList.ts
@@ -12,4 +12,24 @@ export const stampList: StampList = {
         ext: "png",
         fullpath: "class.png",
     },
+    class1: {
+        name: "class1",
+        ext: "png",
+        fullpath: "class1.png",
+    },
+    class2: {
+        name: "class2",
+        ext: "png",
+        fullpath: "class2.png",
+    },
+    class3: {
+        name: "class3",
+        ext: "png",
+        fullpath: "class3.png",
+    },
+    class4: {
+        name: "class4",
+        ext: "png",
+        fullpath: "class4.png",
+    },
 };
