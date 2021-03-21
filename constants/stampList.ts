605d0ec8b84c4e55261e1f9270bfeee66550bbda
m19e <nexus.me7290@gmail.com>
2021-03-22 03:43:13 +0900

    Add stamps title

--- constants/stampList.ts
+++ constants/stampList.ts
@@ -1,6 +1,7 @@
 import { Stamp, StampList } from "types";
 
 export const defaultStamp: Stamp = {
+    title: "twista",
     name: "twista",
     ext: "png",
     fullpath: "twista.png",
@@ -33,66 +34,79 @@ export const stampList: StampList = {
     //     fullpath: "class4.png",
     // },
     askr_nice: {
+        title: "いいね",
         name: "askr_nice",
         ext: "png",
         fullpath: "askr_nice.png",
     },
     askr_help: {
+        title: "わからん",
         name: "askr_help",
         ext: "png",
         fullpath: "askr_help.png",
     },
     askr_hgcsorry: {
+        title: "ごめん樋口",
         name: "askr_hgcsorry",
         ext: "png",
         fullpath: "askr_hgcsorry.png",
     },
     hgc_oko: {
+        title: "は？",
         name: "hgc_oko",
         ext: "png",
         fullpath: "hgc_oko.png",
     },
     hgc_sigh: {
+        title: "はぁ…",
         name: "hgc_sigh",
         ext: "png",
         fullpath: "hgc_sigh.png",
     },
     hgc_empty: {
+        title: "中身のない会話",
         name: "hgc_empty",
         ext: "png",
         fullpath: "hgc_empty.png",
     },
     hgc_greatkit: {
+        title: "偉い、小糸",
         name: "hgc_greatkit",
         ext: "png",
         fullpath: "hgc_greatkit.png",
     },
     kit_no: {
+        title: "ダメだよっ！",
         name: "kit_no",
         ext: "png",
         fullpath: "kit_no.png",
     },
     kit_pya: {
+        title: "ぴゃ…！",
         name: "kit_pya",
         ext: "png",
         fullpath: "kit_pya.png",
     },
     kit_yoyu: {
+        title: "よゆーです！",
         name: "kit_yoyu",
         ext: "png",
         fullpath: "kit_yoyu.png",
     },
     hnn_aha: {
+        title: "あはー？",
         name: "hnn_aha",
         ext: "png",
         fullpath: "hnn_aha.png",
     },
     hnn_yaha: {
+        title: "やは～♡",
         name: "hnn_yaha",
         ext: "png",
         fullpath: "hnn_yaha.png",
     },
     hnn_complain: {
+        title: "え～？",
         name: "hnn_complain",
         ext: "png",
         fullpath: "hnn_complain.png",
