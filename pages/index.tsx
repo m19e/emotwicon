5de2175042b2657d8e7e27f48a09d4fb39325d02
m19e <nexus.me7290@gmail.com>
2021-03-22 03:32:39 +0900

    Remove needless

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,17 +24,6 @@ const Home = ({ stamp, stamps }: Props) => (
             twCard="summary"
         />
         <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-            <h1 className="text-6xl font-bold">
-                Welcome to{" "}
-                <a className="text-blue-600" href="https://nextjs.org">
-                    Next.js!
-                </a>
-            </h1>
-
-            <p className="mt-3 text-2xl">
-                Get started by editing <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">pages/index.js</code>
-            </p>
-
             <p className="mt-3 text-2xl">
                 <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">{JSON.stringify(stamp)}</code>
             </p>
