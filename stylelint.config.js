1ea71b2e0fa4af945f16e5b56d089392a0a7ef9b
m19e <nexus.me7290@gmail.com>
2021-03-22 19:40:39 +0900

    Add unit to stylelint whitelist

--- stylelint.config.js
+++ stylelint.config.js
@@ -8,6 +8,6 @@ module.exports = {
             },
         ],
         "block-no-empty": null,
-        "unit-whitelist": ["em", "rem", "s"],
+        "unit-whitelist": ["em", "rem", "s", "vw"],
     },
 };
