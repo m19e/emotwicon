da3b2da72959ebc26e6f939258c2f4e6d067548f
m19e <nexus.me7290@gmail.com>
2021-03-22 19:22:59 +0900

    Remove padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,7 +23,7 @@ const Home = ({ stamp, stamps }: Props) => (
             twUrl={process.env.NEXT_PUBLIC_SITE_ROOT_URL}
             twCard="summary"
         />
-        <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
+        <main className="flex flex-col items-center justify-center flex-1 text-center">
             <p className="mt-3 text-2xl">{/* <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">{JSON.stringify(stamp)}</code> */}</p>
 
             <div className="grid grid-cols-2 gap-4">
