de5bcf6a0aa07d90ef2ff6775f3dc6c992ccd732
m19e <nexus.me7290@gmail.com>
2021-03-23 00:58:58 +0900

    Remove commented sample stamps

--- constants/stampList.ts
+++ constants/stampList.ts
@@ -9,31 +9,6 @@ export const defaultStamp: Stamp = {
 };
 
 export const stampList: StampList = {
-    // class: {
-    //     name: "class",
-    //     ext: "png",
-    //     fullpath: "class.png",
-    // },
-    // class1: {
-    //     name: "class1",
-    //     ext: "png",
-    //     fullpath: "class1.png",
-    // },
-    // class2: {
-    //     name: "class2",
-    //     ext: "png",
-    //     fullpath: "class2.png",
-    // },
-    // class3: {
-    //     name: "class3",
-    //     ext: "png",
-    //     fullpath: "class3.png",
-    // },
-    // class4: {
-    //     name: "class4",
-    //     ext: "png",
-    //     fullpath: "class4.png",
-    // },
     askr_nice: {
         title: "いいね",
         name: "askr_nice",
