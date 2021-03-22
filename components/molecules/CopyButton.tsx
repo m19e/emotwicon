3a7d13118f37729193bb52dfe0c425cf43055ebd
m19e <nexus.me7290@gmail.com>
2021-03-23 04:17:18 +0900

    Set check icon color

--- components/molecules/CopyButton.tsx
+++ components/molecules/CopyButton.tsx
@@ -12,7 +12,7 @@ const CopyButton = ({ text }: Props) => {
         <CopyToClipboard text={text} onCopy={() => setCopied(true)}>
             {copied ? (
                 <span className="py-1.5 mt-1 rounded-md shadow bg-gray-200 flex-center">
-                    <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <svg className="w-5 h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="#1da1f2">
                         <path
                             fillRule="evenodd"
                             d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
