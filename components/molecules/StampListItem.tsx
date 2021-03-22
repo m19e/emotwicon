508edd45500e3fbcd56564867ef92f6ce7ebb8c0
m19e <nexus.me7290@gmail.com>
2021-03-22 21:25:51 +0900

    Display stamp menu

--- components/molecules/StampListItem.tsx
+++ components/molecules/StampListItem.tsx
@@ -7,7 +7,14 @@ type Props = {
 
 const StampListItem = ({ stamp }: Props) => (
     <div className="w-44 h-44 flex-center rounded-lg border border-gray-200" style={{ maxWidth: "45vw" }}>
-        <Img src={`/stamps/${stamp.fullpath}`} alt={stamp.name} width={144} height={144} />
+        <div className="relative">
+            <Img src={`/stamps/${stamp.fullpath}`} alt={stamp.name} width={144} height={144} />
+            <div className="absolute inset-0 z-10 flex-center flex-col bg-gray-300 opacity-0 hover:opacity-100">
+                <span>Twitterでつぶやく</span>
+                <span>コピーする</span>
+                <span>{"by {スタンプ作者}"}</span>
+            </div>
+        </div>
     </div>
 );
 
