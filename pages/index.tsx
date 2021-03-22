6b7cbd30fbef1dbf062f99beb11f9b90c5994388
m19e <nexus.me7290@gmail.com>
2021-03-23 02:58:42 +0900

    Set site author on footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,15 +31,8 @@ const Home = ({ stamp, stamps }: Props) => (
             </div>
         </main>
 
-        <footer className="flex items-center justify-center w-full h-24 mt-4 border-t">
-            <a
-                className="flex items-center justify-center"
-                href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Powered by <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-            </a>
+        <footer className="flex items-center justify-center w-full h-24 mt-2 border-t">
+            <span className="font-sans">by github:m19e</span>
         </footer>
     </div>
 );
