e7059432cdb83728874194bee5e0f5a219d7ff90
m19e <nexus.me7290@gmail.com>
2021-03-22 21:13:33 +0900

    Set flex-center

--- components/molecules/StampListItem.tsx
+++ components/molecules/StampListItem.tsx
@@ -6,7 +6,7 @@ type Props = {
 };
 
 const StampListItem = ({ stamp }: Props) => (
-    <div className="w-44 h-44 rounded-lg border border-gray-200" style={{ maxWidth: "45vw" }}>
+    <div className="w-44 h-44 flex-center rounded-lg border border-gray-200" style={{ maxWidth: "45vw" }}>
         <Img src={`/stamps/${stamp.fullpath}`} alt={stamp.name} width={144} height={144} />
     </div>
 );
