6681a37c96894fb88df5c95eb01788864339aef3
m19e <nexus.me7290@gmail.com>
2021-03-23 01:44:04 +0900

    Adjust rounded

--- components/molecules/StampListItem.tsx
+++ components/molecules/StampListItem.tsx
@@ -6,17 +6,17 @@ type Props = {
 };
 
 const StampListItem = ({ stamp }: Props) => (
-    <div className="w-44 h-44 flex-center rounded-lg border border-gray-200" style={{ maxWidth: "45vw" }}>
+    <div className="w-44 h-44 flex-center rounded-2xl border border-gray-200" style={{ maxWidth: "45vw" }}>
         <div className="relative group">
             <div className="transition-opacity group-hover:opacity-50">
                 <Img src={`/stamps/${stamp.fullpath}`} alt={stamp.name} width={144} height={144} />
             </div>
             <div className="absolute inset-0 z-10 flex-center flex-col bg-transparent transition-opacity opacity-0 group-hover:opacity-100">
                 <div className="h-1/2 w-full flex flex-col justify-end">
-                    <span className="bg-blue-300 hover:bg-blue-400">ツイートする</span>
+                    <span className="rounded-md bg-blue-300 hover:bg-blue-400">ツイートする</span>
                 </div>
                 <div className="h-1/2 w-full flex flex-col justify-between">
-                    <span className="bg-gray-200 hover:bg-gray-300">コピーする</span>
+                    <span className="rounded-md bg-gray-200 hover:bg-gray-300">コピーする</span>
                     <span className="text-xs">by @{stamp.author}</span>
                 </div>
             </div>
