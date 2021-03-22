d0745a4cf32dbbb527c2b4f82e10940a350a8aba
m19e <nexus.me7290@gmail.com>
2021-03-22 21:00:29 +0900

    Remove debug markup

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,8 +24,6 @@ const Home = ({ stamp, stamps }: Props) => (
             twCard="summary"
         />
         <main className="flex flex-col items-center justify-center flex-1 text-center">
-            <p className="mt-3 text-2xl">{/* <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">{JSON.stringify(stamp)}</code> */}</p>
-
             <div className="grid grid-cols-2 gap-2">
                 {stamps.map((s, i) => (
                     <StampListItem key={i} stamp={s} />
