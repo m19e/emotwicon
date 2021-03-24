3e455395c6f8659598a99610fb159e7d6f699560
m19e <nexus.me7290@gmail.com>
2021-03-24 15:38:15 +0900

    Import module as short name

--- components/molecules/StampList.tsx
+++ components/molecules/StampList.tsx
@@ -1,6 +1,6 @@
 import { Stamp } from "types";
-import DefaultStampListItem from "components/molecules/StampListItem/Default";
-import MobileStampListItem from "components/molecules/StampListItem/Mobile";
+import DefaultStamp from "components/molecules/StampListItem/Default";
+import MobileStamp from "components/molecules/StampListItem/Mobile";
 
 type Props = {
     stamps: Stamp[];
@@ -10,7 +10,7 @@ type Props = {
 const StampList = ({ stamps, touchable }: Props) => (
     <div className="grid gap-2 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
         {stamps.map((s, i) => (
-            <div key={i}>{touchable ? <MobileStampListItem stamp={s} /> : <DefaultStampListItem stamp={s} />}</div>
+            <div key={i}>{touchable ? <MobileStamp stamp={s} /> : <DefaultStamp stamp={s} />}</div>
         ))}
     </div>
 );
