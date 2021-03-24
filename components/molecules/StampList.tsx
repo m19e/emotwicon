27d31049472be350e68444e118eeb00c9080f9e6
m19e <nexus.me7290@gmail.com>
2021-03-24 11:32:11 +0900

    Add stamp list mol

---
+++ components/molecules/StampList.tsx
@@ -0,0 +1,18 @@
+import DefaultStampListItem from "components/molecules/StampListItem/Default";
+import MobileStampListItem from "components/molecules/StampListItem/Mobile";
+import { Stamp } from "types";
+
+type Props = {
+    stamps: Stamp[];
+    touchable: boolean;
+};
+
+const StampList = ({ stamps, touchable }: Props) => (
+    <div className="grid gap-2 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+        {stamps.map((s, i) => (
+            <div key={i}>{touchable ? <MobileStampListItem stamp={s} /> : <DefaultStampListItem stamp={s} />}</div>
+        ))}
+    </div>
+);
+
+export default StampList;
