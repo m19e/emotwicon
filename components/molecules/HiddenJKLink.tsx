16e8695f08758a81d08b48954798cc401f5f3baf
m19e <nexus.me7290@gmail.com>
2021-03-25 04:28:42 +0900

    Dont recieve href

--- components/molecules/HiddenJKLink.tsx
+++ components/molecules/HiddenJKLink.tsx
@@ -1,22 +1,18 @@
 import Link from "next/link";
 import { useState } from "react";
 
-type Props = {
-    href: string;
-};
-
-const HiddenJKLink = ({ href }: Props) => {
+const HiddenJKLink = () => {
     const [show, setShow] = useState(false);
 
     return (
         <span className="font-sans text-lg text-gray-100">
             {show ? (
-                <Link href={href}>
+                <Link href="/jk">
                     <a className="border-b border-gray-100">JKスタンプ→</a>
                 </Link>
             ) : (
                 <>
-                    <span className="cursor-pointer" onClick={() => setShow(true)}>
+                    <span className="mr-1.5 cursor-pointer" onClick={() => setShow(true)}>
                         by
                     </span>
                     <a
