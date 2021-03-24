64f1140f7b3fa4c2fe3df0a95611003d17b92b33
m19e <nexus.me7290@gmail.com>
2021-03-24 11:45:16 +0900

    Replace by template

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Stamp } from "types";
 import { stampList, defaultStamp } from "constants/stampList";
 import MetaHeader from "foundations/MetaHeader";
 import StampList from "components/molecules/StampList";
+import Home from "components/templates/Home";
 
 type Props = {
     stamp: Stamp;
@@ -11,44 +12,7 @@ type Props = {
     touchable: boolean;
 };
 
-const Emotwicon = ({ stamp, stamps, touchable }: Props) => (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <MetaHeader
-            title="#emotwicon ツイッターで使える絵文字・スタンプ"
-            description="#emotwicon"
-            ogTitle={stamp.title}
-            ogDescription="#emotwicon"
-            ogImage={process.env.NEXT_PUBLIC_SITE_ROOT_URL + `/stamps/${stamp.fullpath}`}
-            twTitle={stamp.title}
-            twDescription="#emotwicon"
-            twImage={process.env.NEXT_PUBLIC_SITE_ROOT_URL + `/stamps/${stamp.fullpath}`}
-            twUrl={process.env.NEXT_PUBLIC_SITE_ROOT_URL}
-            twCard="summary"
-        />
-
-        <header className="flex items-center justify-center w-full h-24 mb-2 bg-twitter border-b">
-            <span className="font-sans font-black text-4xl text-gray-100">#emotwicon</span>
-        </header>
-
-        <main className="flex flex-col items-center justify-center flex-1 text-center">
-            <StampList stamps={stamps} touchable={touchable} />
-        </main>
-
-        <footer className="flex items-center justify-center w-full h-24 mt-2 bg-twitter border-t">
-            <span className="font-sans text-lg text-gray-100">
-                by{" "}
-                <a
-                    className="border-b border-opacity-0 border-gray-100 hover:border-opacity-100"
-                    href="https://github.com/m19e"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    m19e
-                </a>
-            </span>
-        </footer>
-    </div>
-);
+const Emotwicon = ({ stamp, stamps, touchable }: Props) => <Home stamp={stamp} stamps={stamps} touchable={touchable} />;
 
 export const getServerSideProps = ({ query, req }: GetServerSidePropsContext): GetServerSidePropsResult<Props> => {
     const check = query.stamp;
