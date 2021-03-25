11fe70acf098adefdc2889c6040840cbb264853a
m19e <nexus.me7290@gmail.com>
2021-03-26 00:09:19 +0900

    Update border len by click

--- components/molecules/HiddenLinkTitle.tsx
+++ components/molecules/HiddenLinkTitle.tsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
+
 type Props = {
     href: string;
 };
 
 const HiddenLinkTitle = ({ href }: Props) => {
-    return <span className="font-sans font-black text-4xl text-gray-100">#emotwicon</span>;
+    const [count, setCount] = useState(0);
+
+    return (
+        <div className="inline-flex font-sans font-black text-4xl text-white">
+            <span
+                className={"border-b border-white border-opacity-0 hover:border-opacity-100 " + (count > 0 ? "border-opacity-100" : "")}
+                onClick={() => setCount((prev) => ++prev)}
+            >
+                #
+            </span>
+            <span className={count > 1 ? "border-b border-white" : ""}>em</span>
+            <span className={count > 2 ? "border-b border-white" : ""}>otw</span>
+            <span className={count > 3 ? "border-b border-white" : ""}>icon</span>
+        </div>
+    );
 };
 
 export default HiddenLinkTitle;
