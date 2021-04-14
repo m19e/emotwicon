8c757795c861fd1ab44c8dedd76505d8265a725a
m19e <nexus.me7290@gmail.com>
2021-04-14 18:05:01 +0900

    Remove / from share url

--- components/molecules/StampListItem/Default.tsx
+++ components/molecules/StampListItem/Default.tsx
@@ -18,7 +18,7 @@ const Default = ({ stamp }: Props) => (
                         className="py-1.5 mb-1 rounded-md shadow text-white text-sm font-bold font-sans bg-twitter"
                         href={
                             "https://twitter.com/intent/tweet?text=" +
-                            encodeURIComponent("#emotwicon " + process.env.NEXT_PUBLIC_SITE_ROOT_URL + "/?stamp=" + stamp.name)
+                            encodeURIComponent("#emotwicon " + process.env.NEXT_PUBLIC_SITE_ROOT_URL + "?stamp=" + stamp.name)
                         }
                         target="_blank"
                         rel="noopener noreferrer"
@@ -27,7 +27,7 @@ const Default = ({ stamp }: Props) => (
                     </a>
                 </div>
                 <div className="h-1/2 w-full flex flex-col justify-start">
-                    <CopyButton text={"#emotwicon " + process.env.NEXT_PUBLIC_SITE_ROOT_URL + "/?stamp=" + stamp.name} />
+                    <CopyButton text={"#emotwicon " + process.env.NEXT_PUBLIC_SITE_ROOT_URL + "?stamp=" + stamp.name} />
                     {/* <span className="text-xs text-gray-700 font-bold">by @{stamp.author}</span> */}
                 </div>
             </div>
