7b3b2fcc7b283ccc2333c6c4580aea16697d77d5
m19e <nexus.me7290@gmail.com>
2021-06-15 01:02:16 +0900

    Add stamp to list

--- constants/stampList.ts
+++ constants/stampList.ts
@@ -278,6 +278,13 @@ export const JKstampList: StampList = {
         fullpath: "jk/flare_ew.png",
         author: "Flame_tsukushi4",
     },
+    yomotsu_whistle: {
+        title: "ピピ～ッ！",
+        name: "yomotsu_whistle",
+        ext: "png",
+        fullpath: "jk/yomotsu_whistle.png",
+        author: "ameno_tokotachi",
+    },
     yomotsu_mmm: {
         title: "ムーー",
         name: "yomotsu_mmm",
