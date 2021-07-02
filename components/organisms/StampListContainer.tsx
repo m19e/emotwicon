11e2e145428a5dbd2e00b2cf3a1b4334a3d12bfc
m19e <nexus.me7290@gmail.com>
2021-07-02 20:21:04 +0900

    Pass toggle method to list

--- components/organisms/StampListContainer.tsx
+++ components/organisms/StampListContainer.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Stamp } from "types";
-import StampList from "components/molecules/JKstampList";
+import StampList from "components/molecules/stampListJkWithFav";
 
 type Props = {
     stamps: Stamp[];
@@ -20,12 +20,25 @@ const StampListContainer = ({ stamps, touchable }: Props) => {
         }
     }, []);
 
-    const toggleStampFav = () => {};
+    const toggleStampFav = (s: Stamp) => {
+        setFavIds((prev) => {
+            const filtered = prev.filter((id) => id !== s.name);
+            return filtered;
+        });
+    };
 
     return (
         <main className="flex flex-col items-center justify-center flex-1 text-center">
             <button onClick={() => setFavMode((prev) => !prev)}>お気に入り:{favMode ? "有効" : "無効"}</button>
-            <StampList stamps={stamps} touchable={touchable} />
+            {favMode ? (
+                <StampList
+                    stamps={stamps.map((s) => Object.assign(s, { fav: favIds.includes(s.name) })).filter((s) => s.fav)}
+                    touchable={touchable}
+                    toggle={toggleStampFav}
+                />
+            ) : (
+                <StampList stamps={stamps.map((s) => Object.assign(s, { fav: favIds.includes(s.name) }))} touchable={touchable} toggle={toggleStampFav} />
+            )}
         </main>
     );
 };
