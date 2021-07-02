969c130cd445ec6f38f20e3378f0148c70f160af
m19e <nexus.me7290@gmail.com>
2021-07-02 19:50:36 +0900

    Add mol for jk stamp with fav

--- components/molecules/StampListItem/Default.tsx
+++ components/molecules/StampListItem/Default.tsx
@@ -64,4 +64,33 @@ export const DefaultJK = ({ stamp }: Props) => (
     </div>
 );
 
+export const DefaultJkWithFav = ({ stamp }: Props) => (
+    <div className="w-44 h-44 max-w-full flex-center rounded-2xl bg-white border-2 border-gray-200">
+        <div className="relative group">
+            <div className="flex-center transition-opacity group-hover:opacity-25">
+                <Img src={`/stamps/${stamp.fullpath}`} alt={stamp.name} width={144} height={144} />
+            </div>
+            <div className="absolute inset-0 z-10 flex-center flex-col bg-transparent transition-opacity opacity-0 group-hover:opacity-100">
+                <div className="h-1/2 w-full flex flex-col justify-end">
+                    <a
+                        className="py-1.5 mb-1 rounded-md shadow text-white text-sm font-bold font-sans bg-twitter"
+                        href={
+                            "https://twitter.com/intent/tweet?text=" +
+                            encodeURIComponent("#emotwicon " + process.env.NEXT_PUBLIC_SITE_ROOT_URL + "/jk?stamp=" + stamp.name)
+                        }
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        ツイートする
+                    </a>
+                </div>
+                <div className="h-1/2 w-full flex flex-col justify-start">
+                    <CopyButton text={"#emotwicon " + process.env.NEXT_PUBLIC_SITE_ROOT_URL + "/jk?stamp=" + stamp.name} />
+                    {/* <span className="text-xs text-gray-700 font-bold">by @{stamp.author}</span> */}
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
 export default Default;
