43748ec15ec386e1a705b7789d633a1a5b52b5f6
m19e <nexus.me7290@gmail.com>
2021-07-02 20:20:30 +0900

    Add stamplist mol for handle fav

---
+++ components/molecules/stampListJkWithFav.tsx
@@ -0,0 +1,29 @@
+import { Stamp } from "types";
+import { DefaultJkWithFav } from "components/molecules/StampListItem/Default";
+import { MobileJkWithFav } from "components/molecules/StampListItem/Mobile";
+
+type Props = {
+    stamps: Stamp[];
+    touchable: boolean;
+    toggle: (s: Stamp) => void;
+};
+
+const StampList = ({ stamps, touchable, toggle }: Props) => (
+    <div className="w-11/12 grid gap-2 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+        {touchable ? (
+            <>
+                {stamps.map((s, i) => (
+                    <MobileJkWithFav key={i} stamp={s} />
+                ))}
+            </>
+        ) : (
+            <>
+                {stamps.map((s, i) => (
+                    <DefaultJkWithFav key={i} stamp={s} />
+                ))}
+            </>
+        )}
+    </div>
+);
+
+export default StampList;
