2f2a85e824bc8a3c3d79594db52dc3c7c955d8bd
m19e <nexus.me7290@gmail.com>
2021-07-02 19:36:18 +0900

    Toggle fav mode

--- components/organisms/StampListContainer.tsx
+++ components/organisms/StampListContainer.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Stamp } from "types";
 import StampList from "components/molecules/JKstampList";
 
@@ -7,8 +8,23 @@ type Props = {
 };
 
 const StampListContainer = ({ stamps, touchable }: Props) => {
+    const [favMode, setFavMode] = useState(false);
+    const [favIds, setFavIds] = useState<string[]>([]);
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            const res = localStorage.getItem("emotwicon/fav/jk");
+            const data: string[] = res === null ? [] : JSON.parse(res);
+            console.log(res, data);
+            setFavIds(data);
+        }
+    }, []);
+
+    const toggleStampFav = () => {};
+
     return (
         <main className="flex flex-col items-center justify-center flex-1 text-center">
+            <button onClick={() => setFavMode((prev) => !prev)}>お気に入り:{favMode ? "有効" : "無効"}</button>
             <StampList stamps={stamps} touchable={touchable} />
         </main>
     );
