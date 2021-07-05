0a8fef271fd5da9aa25612791b0583c23758827b
m19e <nexus.me7290@gmail.com>
2021-07-05 19:40:11 +0900

    Set toggle button to switch fav mode

--- components/organisms/StampListContainer.tsx
+++ components/organisms/StampListContainer.tsx
@@ -53,7 +53,26 @@ const StampListContainer = ({ stamps, touchable }: Props) => {
 
     return (
         <main className="flex flex-col items-center flex-1 text-center">
-            <button onClick={() => setFavMode((prev) => !prev)}>お気に入り:{favMode ? "有効" : "無効"}</button>
+            {/* <button onClick={() => setFavMode((prev) => !prev)}>お気に入り:{favMode ? "有効" : "無効"}</button> */}
+            <div className="w-11/12 mb-2 inline-flex justify-end items-center">
+                <button className="relative cursor-pointer" onClick={() => setFavMode((prev) => !prev)}>
+                    <span className={"block w-10 h-6 bg-gray-200 rounded-full shadow-inner transition-colors" + (favMode ? " bg-yellow-300" : "")}></span>
+                    <span
+                        className={
+                            "absolute block w-4 h-4 mt-1 ml-1 bg-white rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transition-transform duration-300 ease-in-out" +
+                            (favMode ? " transform translate-x-full" : "")
+                        }
+                    ></span>
+                </button>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-1 stroke-current fill-current text-yellow-300" viewBox="0 0 24 24" fill="none">
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                    />
+                </svg>
+            </div>
             {favMode ? (
                 <StampList stamps={favStamps} touchable={touchable} toggle={toggleStampFav} />
             ) : (
