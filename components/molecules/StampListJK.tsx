6cd4296b32514657a247c953ecadcad8a36e2544
m19e <nexus.me7290@gmail.com>
2021-07-14 00:01:36 +0900

    Check fav in stamp list

--- components/molecules/StampListJK.tsx
+++ components/molecules/StampListJK.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 const StampList = ({ stamps, toggle, touchable, fav }: Props) => (
     <div className="w-11/12 mb-2 grid gap-3 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
-        {stamps.length === 0 && (
+        {stamps.length === 0 && fav && (
             <div className="w-44 h-44 max-w-full flex-center rounded-2xl bg-white border-2 border-gray-200">
                 <span className="text-gray-400 font-bold font-sans">スタンプ未登録</span>
             </div>
