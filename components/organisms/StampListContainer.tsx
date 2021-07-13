4b51fb6197966c6da66e6945d7fd3f50acaa9605
m19e <nexus.me7290@gmail.com>
2021-07-14 04:56:20 +0900

    Rotate fav-button

--- components/organisms/StampListContainer.tsx
+++ components/organisms/StampListContainer.tsx
@@ -94,23 +94,27 @@ const StampListContainer = ({ stamps, touchable, page }: Props) => {
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
                         </svg>
                     </button>
-                    <button className="relative mx-1.5 cursor-pointer" onClick={() => setFavMode((prev) => !prev)}>
-                        <span className={"block w-14 h-8 bg-gray-200 rounded-full shadow-inner transition-colors " + (favMode ? "bg-yellow-300" : "")}></span>
-                        <span
-                            className={
-                                "absolute block w-6 h-6 mt-1 ml-1 bg-white rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transition-transform duration-300 ease-in-out " +
-                                (favMode ? "transform translate-x-full" : "")
-                            }
-                        ></span>
-                    </button>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 stroke-current fill-current text-yellow-300" viewBox="0 0 24 24" fill="none">
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth={2}
-                            d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
-                        />
-                    </svg>
+                    <div className={"inline-flex items-center transform transition-transform duration-700 " + (leftHanded ? "rotate-180" : "rotate-0")}>
+                        <button className="relative mx-1.5 cursor-pointer" onClick={() => setFavMode((prev) => !prev)}>
+                            <span
+                                className={"block w-14 h-8 bg-gray-200 rounded-full shadow-inner transition-colors " + (favMode ? "bg-yellow-300" : "")}
+                            ></span>
+                            <span
+                                className={
+                                    "absolute block w-6 h-6 mt-1 ml-1 bg-white rounded-full shadow inset-y-0 left-0 focus-within:shadow-outline transition-transform duration-300 ease-in-out " +
+                                    (favMode ? "transform translate-x-full" : "")
+                                }
+                            ></span>
+                        </button>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 stroke-current fill-current text-yellow-300" viewBox="0 0 24 24" fill="none">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={2}
+                                d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z"
+                            />
+                        </svg>
+                    </div>
                 </div>
             </div>
             {page === "default" && <StampList stamps={favMode ? favStamps : rootStamps} toggle={toggleStampFav} touchable={touchable} fav={favMode} />}
