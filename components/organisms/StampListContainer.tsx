6b9c572bfbe749e2c9a7541c3f9001135b65c13a
m19e <nexus.me7290@gmail.com>
2021-07-13 23:58:13 +0900

    Pass fav mode prop

--- components/organisms/StampListContainer.tsx
+++ components/organisms/StampListContainer.tsx
@@ -75,7 +75,7 @@ const StampListContainer = ({ stamps, touchable, page }: Props) => {
                 </svg>
             </div>
             {page === "default" && <StampList stamps={favMode ? favStamps : rootStamps} touchable={touchable} toggle={toggleStampFav} />}
-            {page === "jk" && <StampListJK stamps={favMode ? favStamps : rootStamps} touchable={touchable} toggle={toggleStampFav} />}
+            {page === "jk" && <StampListJK stamps={favMode ? favStamps : rootStamps} toggle={toggleStampFav} touchable={touchable} fav={favMode} />}
         </main>
     );
 };
